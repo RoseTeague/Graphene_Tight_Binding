0831cd6fd22bb7e759a6239a312a150149e91320
Zachary Goodwin <zachary.goodwin13@imperial.ac.uk>
2018-01-29 09:12:06 +0000

    removed the hamiltonian construction

--- DD_TB.py
+++ DD_TB.py
@@ -1,6 +1,10 @@
+"""Module for solver of with TB split operator
+"""
 import math #can not remeber if this is how we do this ...
 import numpy as np
 import matplotlib as mpl
+from scipy import constants
+import scipy.linalg
 #import scipy.fftpack ... this might be a little awkward
 
 #this is for the solver part ...
@@ -14,30 +18,31 @@ def TB_solver(dt,DT):
 
     #probably want to have a file with all of the details ...
     Ns = DT/dt #need to then round this off to the nearest integer ...
-
-    HI = -2.7#Hopping integral in terms of eV
-
-    #can also work out the matrix element that we will multiply by to solve the
-    #propogation of the wave packet ... this is math.i*math.e/math.hbar ...
+    N = 100
+    n = 10
 
     #should call the wavefunction ...
     #need to specify how it is constructed so we can make the tridagonly matrices
+    wvf = np.ones((N,1))#wavefunction ... just put some matrix there for now ...
 
-    #construct the first tri diagonal matrix ...
-    #two matrices are required here ...
+    #then need to call the hamiltonian ...
 
-    #construct the second tri diagonal matrix ...
-    #two matrices are required here ...
-
-    for i in range(Ns):
+    #for i in range(Ns):
 
         #matrix multiply the wavefunctino by the + version of the first tri diagonal
+
+        #https://stackoverflow.com/questions/44388358/python-numpy-matrix-multiplication-with-one-diagonal-matrix
+        #c = (a*b.T).T ... fastest way and we have it in this form ...
 
         #solve the linear equation with the - version of the first tri diagonal and
         #the vector just calcualted
 
+        # scipy.linalg.solve_banded((1,1), m_packed, b) ... not sure what the first tupel is
+
         #Now we need to shift the result around such that another tri diagonal
         #matrix equation can be solved
+        #might be able to do this with the range thing from above ...
+        #should be able to do this with something similar to DD_TBH
 
         #After we have reordered the result, we multiply with the + version of the
         #second tri diagonal
