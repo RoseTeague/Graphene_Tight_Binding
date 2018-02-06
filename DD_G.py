dd3edc5741a30037c15b52910275ce0f0e19ae46
Georgios <georgios.samaras17@ic.ac.uk>
2018-02-06 11:59:12 +0000

    changes including potential

--- DD_G.py
+++ DD_G.py
@@ -12,7 +12,7 @@ from scipy.sparse import dia_matrix
 import DD_WP_G
 from DD_WP_G import *
 
-def TBH(n=10,m=10,dt=0.1e-15):
+def TBH(DP, n=10,m=10,dt=0.1e-15, V=False):
     """
     """
 
@@ -24,13 +24,50 @@ def TBH(n=10,m=10,dt=0.1e-15):
 
     #Constants that populate the off diagonal elements of the hamiltonian
     H_1 = (HI*1j*constants.e*dt)/constants.hbar
+    H_V = 1j*constants.e*dt/constants.hbar
 
     #Constructing the set of tridiagonal matrices for H_m
     #H = np.zeros((3,n),dtype=complex)
 
-    H_d = np.full(N,0)
-    H_cd_u = np.full(N-1,-H_1)#.reshape((n*m-1,1))
-    H_cd_l = np.full(N-1,-H_1)
+    H_d = np.full(N,0,dtype=complex)
+    
+    if V:
+
+        #call the potential function ...
+#        DP = oneDdisorderpotential(m,n)
+
+        #Initial counters are required to populate Hamiltonian
+        ip = 0
+        fp = n
+
+        #Need to loop over each column of carbon atoms to set the disorder potential
+        for j in range(m):
+
+            #The even and odd column numbers are populated from the disorder potential
+            #differently, because the atoms are in a zig zag.
+            if j % 2 == 0:
+
+                H_d[ip:fp:2] = H_V*DP[2*j+1,0]
+                H_d[ip:fp:2] = H_V*DP[2*j+1,0]
+
+                H_d[ip+1:fp+1:2] = H_V*DP[2*j,0]
+                H_d[ip+1:fp+1:2] = H_V*DP[2*j,0]
+
+            else:
+
+                H_d[ip:fp:2] = H_V*DP[2*j,0]
+                H_d[ip:fp:2] = H_V*DP[2*j,0]
+
+                H_d[ip+1:fp+1:2] = H_V*DP[2*j+1,0]
+                H_d[ip+1:fp+1:2] = H_V*DP[2*j+1,0]
+
+            #conting to the next set of atoms
+            ip += n
+            fp += n
+
+    
+    H_cd_u = np.full(N-1,-H_1,dtype=complex)#.reshape((n*m-1,1))
+    H_cd_l = np.full(N-1,-H_1,dtype=complex)
 
     #need to check these ... might not actually need two of these ...
     #Should this be n or m ... ?
@@ -45,12 +82,11 @@ def TBH(n=10,m=10,dt=0.1e-15):
     H_cd_u[1:N:2] = 0
     H_cd_l[1:N:2] = 0
 
-    H = np.diag(H_d, 0) + np.diag(H_rd_l, -n) + np.diag(H_rd_u, n) + np.diag(H_cd_l, -1) + np.diag(H_cd_u, 1)
-    H = sparse.csr_matrix(H)
+    H = sparse.csr_matrix(np.diag(H_d, 0) + np.diag(H_rd_l, -n) + np.diag(H_rd_u, n) + np.diag(H_cd_l, -1) + np.diag(H_cd_u, 1))
 
     return H
 
-def TB_solver_2D(n, m, pos, wfc, dt, DT):
+def TB_solver_2D(DP, n, m, pos, wfc, dt, DT, V):
     """Split operator technique for propogation of wave packets with square
         lattice tight-binding hamiltonian.
 
@@ -60,9 +96,9 @@ def TB_solver_2D(n, m, pos, wfc, dt, DT):
 
     wvf = wfc
 
-    H  = TBH(n,m,dt=0.1e-15)
+    H  = TBH(DP, n,m,dt,V)
 
-    points = Crystal(m, n)
+    #points = Crystal(m, n)
 
     for i in range(Ns):
 
@@ -73,7 +109,7 @@ def TB_solver_2D(n, m, pos, wfc, dt, DT):
     pd = np.multiply(wvf_c, wvf)
     pd = np.reshape(pd,(n,m))
 
-    plt.contourf(points[0].reshape((n,m)),points[1].reshape((n,m)),pd)#,cmap='RdGy'
+    plt.contourf(pos[0].reshape((n,m)),pos[1].reshape((n,m)),pd)#,cmap='RdGy'
     plt.show()
     
     return pd
