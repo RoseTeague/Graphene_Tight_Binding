470055731639eddf7a1ea170b7061ba8b65bcbee
Amy Wang <yiyuan.wang16@imperial.ac.uk>
2018-02-08 09:40:08 +0000

    Update DD_DP_G.py

--- DD_DP_G.py
+++ DD_DP_G.py
@@ -34,28 +34,33 @@ def oneDdisorderpotential(m,n,lc,pos):
     x_1 = X[0:n*m:n,0]
     x_2 = X[1:n*m:n,0]
 
-    #Generate all x positions in an ascending order
-    x = np.sort(x_1.tolist()+x_2.tolist())
-
     #Parameters for the strength of the disorder potential in units of eV.
     Delta = 0.3
 
     #Generating sample of random numbers of Guassian distribution.
-    V = np.random.normal(0,1,2*m)
+    V = np.random.normal(0,1,m)
 
-    #Generate the two-point matrix
-    X1 = np.tile(x,(2*m,1))
+    #Generate the two-point matrix for two rows along x axis
+    X1 = np.tile(x_1,(m,1))
     X2 = X1.T
+    X3 = np.tile(x_2,(m,1))
+    X4 = X3.T
 
-    #Generate the two-point spatial correlation matrix
-    C = Delta**2*np.exp(-abs(X1-X2)/lc)
+    #Generate the two-point spatial correlation matrix for two rows
+    C1 = Delta**2*np.exp(-abs(X1-X2)/lc)
+    C2 = Delta**2*np.exp(-abs(X3-X4)/lc)
 
     #Cholesky decomposition of the two-point correlation matrix and generate the final random vector
-    L = np.linalg.cholesky(C)
-    W = np.dot(L,V)
+    L1 = np.linalg.cholesky(C1)
+    W1 = np.dot(L1,V)
+    L2 = np.linalg.cholesky(C2)
+    W2 = np.dot(L2,V)
 
-    #Reshaping for further calculation.
-    Wfinal = W.reshape((2*m,1))
+    #Reshaping for further calculations.
+    Wf=np.zeros(n,m)
+    Wf[0:n:2,0:m]=W1 
+    Wf[1:n:2,0:m]=W2
+    Wfinal=Wf.T.reshape((n*m,1))
 
     return Wfinal
 
