66e870886072e347ca2871679d9d18f92b3d0b46
Amy Wang <yiyuan.wang16@imperial.ac.uk>
2018-02-09 22:18:13 +0000

    Update DD_DP_G2.py

--- DD_DP_G2.py
+++ DD_DP_G2.py
@@ -8,9 +8,29 @@ import numpy as np
 def twoDdisorderpotential(m,n,lc,pos):
     """
     ============================================================================
-    Create one-dimensional disorder potential on graphene lattice
+    Create two-dimensional disorder potential on graphene lattice
     ============================================================================
-
+    
+    This function creates an two-dimensional spatially-correlated Gaussian 
+    disorder potential for graphene. The method used is the same as that in the 
+    paper: Choi, SangKook, Cheol-Hwan Park, and Steven G. Louie. 
+    "Electron supercollimation in graphene and Dirac Fermion materials 
+    using one-dimensional disorder potentials." 
+    Physical review letters 113.2 (2014): 026802. 
+    
+    
+    To be clearer, the two-dimensional spatially-correlated Gaussian disorder 
+    potential can be the sum of two one-dimensional potentials respectively along x
+    and y axis. The one-dimensional potential can be in the form of a random vector 
+    having the two-point spatial correlation property. Hence, firstly, a random vector 
+    consisting of spatially-uncorrelated Gaussian-random variables is composed. 
+    Next, using the positions of atoms taken as input parameters, the two-point
+    spatial correlation matrix is created and Cholesky decomposition method is 
+    used to obtain the matrix with desired spatial correlation property. Finally, 
+    the final vector is the dot product of the random vector and matrix with the 
+    required spatial correlation property.
+    
+    
     Inputs
     ----------
     m : integer
