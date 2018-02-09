26a6d1b4254d6eabd5a39015b8b86d8e5f5b2dcb
Amy Wang <yiyuan.wang16@imperial.ac.uk>
2018-02-09 20:54:24 +0000

    Update DD_DP_S.py

--- DD_DP_S.py
+++ DD_DP_S.py
@@ -22,7 +22,7 @@ def oneDdisorderpotential(m,n,lc,pos):
     lc : float
         correlation length
         
-    pos: list
+    pos: float, list
         A list containing position information of atoms
 
     Returns
