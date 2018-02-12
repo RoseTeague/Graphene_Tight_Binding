9199a0c7c787acba91c9e23ea8e4d1683a8af653
Rosemary <rdt13@ic.ac.uk>
2018-02-12 11:08:00 +0000

    Added doc strings and comments

--- DD_COMP.py
+++ DD_COMP.py
@@ -7,65 +7,104 @@ import matplotlib.pyplot as plt
 
 def Comparison(lattice,V):
     """
+    ==========================================================================
+    Comparison of simple and fast solvers
+    ==========================================================================
+
+    This function calculates the difference in the final wavefunctions when
+    either the simple solver or the fast solver are used to propagate the
+    wavepacket. This is used to validate the different approaches.
+
+    The system parameters on lines ___ to __ can be modified to change the
+    conditions on the system.
+
+    Inputs
+    ---------
+    lattice - string
+            - description of the lattice to be modelled. This can be either
+                'square', 'graphene', or '1D square'.
+
+    V       - string
+            - description of the potential to be used on the system. This can be
+                either 'None', 'one dimensional' or 'two dimensional'.
+                If the lattice is '1D square', the potential MUST be 'None'
+
+    Returns
+    ---------
+    N.A
+
+    Displays a plot of the differences between the two final wavefunctions,
+    found by propagating the system with either the simple or the fast solver.
+
     """
 
-    n = 100
-    m = 100
-    lc = 1
-    s = 5*lc
-    kx = math.pi/(5*lc)
-    ky = math.pi/(5*lc)
-    dt = 0.1e-15
-    T = 0#0.1e-15
-    V = 'no'#'one dimensional'
-    Ns = round(T/dt) + 1
+    # System parameters
+    m = 100                         # Number of atoms along x
+    n = 100                         # Number of atoms along y
+    lc = 1                          # Correlation length of disorder potential
+    s = 5*lc                        # Width of initial gaussian packet
+    kx = math.pi/(5*lc)             # Momentum eigenvalue (wavenumber) along x
+    ky = math.pi/(5*lc)             # Momentum eigenvalue (wavenumber) along y
+    dt = 0.1e-15                    # Time interval to be sampled
+    T = 0#0.1e-15                   # Total time of wavepacket propagation
+    Ns = round(T/dt) + 1            # Integer number of time steps
 
     if lattice == 'square':
+        # Import modules for square lattice
         from DD_WP_S import Crystal                         # Wavefunction module
         from DD_WP_S import Psi                             # Wavefunction module
         from DD_DP_S import oneDdisorderpotential           # Potential module
         from DD_DP_S2 import twoDdisorderpotential          # Potential module
-        from DD_SH import TBH                               # Hamiltonian module        
+        from DD_SH import TBH                               # Hamiltonian module
         from DD_FH_S import FTBH                            # Hamiltonian module
 
     if lattice == 'graphene':
+        # Import modules for graphene lattice
         from DD_WP_G import Crystal                         # Wavefunction module
         from DD_WP_G import Psi                             # Wavefunction module
         from DD_DP_G import oneDdisorderpotential           # Potential module
-        from DD_DP_G2 import twoDdisorderpotential          # Potential module 
+        from DD_DP_G2 import twoDdisorderpotential          # Potential module
         from DD_GH import TBH                               # Hamiltonian module
         from DD_FH_G import FTBH                            # Hamiltonian module
 
+    # Run the system modules to descibe the lattice and initial wavepacket
     pos = Crystal(n,m)
     wfc = Psi(s,kx,ky,n,m,pos)
-    DP = oneDdisorderpotential(m,n,lc,pos)
-    
+
+    # Run the potential module for the system to be simulated
+    if V == 'None':
+        # If the system is to be run with no external potential, assign an
+        # arbitrary value of 1 to the potential variable.
+        DP = 1
+        V = 'no'
+
+    if V == 'one dimensional':
+        DP = oneDdisorderpotential(m,n,lc,pos)
+
     if V == 'two dimensional':
         DP = twoDdisorderpotential(m,n,lc,pos)
 
-
+    # Import and Run the simple solver module
     from DD_SS import TB_ss                                 # Solver module
     FH = FTBH(DP,n,m,dt,V)
 
     wvf_ss = TB_ss(n, m, pos, wfc, FH, T, dt)
 
-    # Import modules for Square lattice
-
+    # Import and Run the full (fast) solver module
     from DD_TB_S import TB_solver_S                         # Solver module
-
     H = TBH(DP,n,m,dt,V)
 
     wvf_S = TB_solver_S(n,m,pos,wfc,H,T,dt,False)
 
+
+    # Calculates the difference between the final wavefunctions from each method.
     difference = wvf_ss - wvf_S
 
-    #print(np.amax(difference)/np.amax(wvf2D)*100)
-
-    #Plotting ... what does the 100 do?
+    #Plotting
     plt.contourf(pos[0].reshape((n,m)),pos[1].reshape((n,m)), difference, 100, cmap = 'gnuplot')
     plt.title('n='+str(n)+' m='+str(m)+' t='+str(Ns*0.1)+'fs')
     plt.show()
 
 if __name__ == '__main__':
-    #TB('Square', 'no')
-    Comparison('square', 'no')
+    #TB('Square', 'None')
+    Comparison('square', 'None')
--- DD_Main.py
+++ DD_Main.py
@@ -1,107 +1,119 @@
 """
 Main program for studying the propagation of a Gaussian wavepacket through 2D
 crystals using the Tight Binding Method.
-
-
-For animations to work, create an empty folder called Images and download cv2
-with the following command:
-                pip install opencv-python
-
-Functions required for this method and their necessary output formats are:
-    Crystal                 :  Purpose - describes the 2D lattice
-                                Output  - X - list of coordinate x-values going down each column
-                                                of the Lattice
-                                        - Y - list of coordinate y-values going down each column
-                                                of the Lattice
-                                        - X0 - initial x value
-                                        - Y0 - initial y value
-    Psi                     :  Purpose - Describes the initial wavepacket
-                                Output  - Psi - complex value of the wavepacket at each
-                                            coordinate described by (X,Y)
-    oneDdisorderpotential   :  Purpose - Define the potential
-                                Output  -
-    TBH                     :  Purpose - Define the Tight Binding Hamiltonian
-                                Output  - TH1P -
-                                        - TH2P -
-                                        - TH1N -
-                                        - TH2N -
 """
 
 import math
 import matplotlib.pyplot as plt
 
 # Describe system for use in image/video titles
-def TB(lattice,V):
+def TB(lattice,V,animate=False):
+    """
+    ===========================================================================
+    Main file for the split operator tight binding solver
+    ===========================================================================
 
-    n = 50#500
-    m = 0#500
-    lc = 1
-    s = 5*lc
-    kx = 1#math.pi/(5*lc)
-    ky = 0#math.pi/(5*lc)
-    dt = 0.1e-15
-    T = 5e-15
-    Ns = round(T/dt) + 1
+    This function collects all modules which define the lattice, the initial
+    wavepacket, the Tight Binding Hamiltonian and the full split operator
+    solver. It applies the same parameters to each module and allows the program
+    to be run from one place.
 
-    #We should just be able to import the solver ... there does not need to be
-    #Any specification on which one it is, since both are the same ...
+    The following variables are defined on lines ___ to ___:
+    the size of the system, the correlation length, the
+    width of the Gaussian packet, the momentum eigenvalues in x and y and the
+    time intervals and length to be simulated can be adjusted. These can be
+    changed to simulate different conditions.
 
+    Inputs
+    -----------
+    lattice - string
+            - description of the lattice to be modelled. This can be either
+                'square' or 'graphene'.
+
+    V       - string
+            - description of the potential to be used on the system. This can be
+                either 'None', 'one dimensional' or 'two dimensional'.
+
+    animate - boolean
+            - Determines whether or not a .mp4 file of the propagation will be
+                produced. N.B this takes more time so should only be set if it
+                is necessary to study the propagation.
+
+    Returns
+    ----------
+    N.A
+
+    If 'animate' is set to be true, an .mp4 file will be saved.
+    A plot of the final state of the wavepacket will be displayed as a contour
+        plot where the colour represents the amplitude.
+    """
+
+    # System parameters: can be modified
+    m = 500                     # Number of atoms along x
+    n = 500                     # Number of atoms along y
+    lc = 1                      # Correlation length for disorder potential
+    s = 5*lc                    # Width of initial Gaussian wavepacket
+    kx = math.pi/(5*lc)         # Momentum eigenvalue (wavenumber) along x
+    ky = math.pi/(5*lc)         # Momentum eigenvalue (wavenumber) along x
+    dt = 0.1e-15                # Time interval to be sampled
+    T = 5e-15                   # Total time of wavepacket propagation
+    Ns = round(T/dt) + 1        # Integer number of time steps
+
+
+    # Import the full (fast) Hamiltonian solver
+    from DD_TB_S import TB_solver_S                         # Solver module
 
     if lattice == 'square':
-        # Import modules for Square lattice
+        # Import system modules for Square lattice
         from DD_WP_S import Crystal                         # Wavefunction module
         from DD_WP_S import Psi                             # Wavefunction module
         from DD_DP_S import oneDdisorderpotential           # Potential module
         from DD_DP_S2 import twoDdisorderpotential          # Potential module
         from DD_SH import TBH                               # Hamiltonian module
-        from DD_TB_S import TB_solver_S                         # Solver module
 
 
     if lattice == 'graphene':
-        # Import modules for Square lattice
+        # Import system modules for Graphene (hexagonal) lattice
         from DD_WP_G import Crystal                         # Wavefunction module
         from DD_WP_G import Psi                             # Wavefunction module
         from DD_DP_G import oneDdisorderpotential           # Potential module
         from DD_DP_G2 import twoDdisorderpotential          # Potential module
         from DD_GH import TBH                               # Hamiltonian module
-        from DD_TB_S import TB_solver_S                         # Solver module
 
 
-    if lattice == '1D square':
-        # Import modules for 1D square lattice
-        from DD_1D_modified import Crystal
-        from DD_1D_modified import Psi
-        from DD_1D_modified import TBH
-        from DD_1D_modified import TB_solver_S
-
-        def oneDdisorderpotential(m,n,lc,pos):
-            return 1
 
         #Need to have some assert statements here ...
 
+    # Run the system modules to descibe the lattice and initial wavepacket
     pos = Crystal(n,m)
     wfc = Psi(s,kx,ky,n,m,pos)
-    #this means that we calcualte it even if we do not need it ... I don't like that
-    #Need to add it so that the two dimensional disorder potential can be chosen here ...
 
-    #Choose the 1D potential first ... if it is not correct, then write over it ...
-    DP = oneDdisorderpotential(m,n,lc,pos)
+    # Run the potential module for the system to be simulated
+    if V == 'None':
+        # If the system is to be run with no external potential, assign an
+        # arbitrary value of 1 to the potential variable.
+        DP = 1
+
+    if V == 'one dimensional':
+        DP = oneDdisorderpotential(m,n,lc,pos)
+
     if V == 'two dimensional':
         DP = twoDdisorderpotential(m,n,lc,pos)
 
+    # Run the Hamiltonian module for the system
     H = TBH(DP,n,m,dt,V)
 
-    #Change this to True to produce an mp4 video
-    animate = True
-
     if animate:
-
+    # Solves the tight binding hamiltonian using a split operator method
+    # described in the file DD_TB_S. Records an image after each time step and
+    # compiles into an mp4 movie file.
         from animate import MakeMovie
 
         pd = TB_solver_S(n,m,pos,wfc,H,T,dt,animate)
         MakeMovie('Tight Binding in ' + lattice + ' with ' + V + ' potential')
     else:
-
+    # Solves the tight binding hamiltonian using a split operator method
+    # describes in the file DD_TB_S
         pd = TB_solver_S(n,m,pos,wfc,H,T,dt,animate)
 
     #Plotting
@@ -109,21 +121,63 @@ def TB(lattice,V):
     # plt.title('n='+str(n)+' m='+str(m)+' t='+str(Ns*0.1)+'fs')
     # plt.show()
 
-def TBS(lattice,V):
+def TBS(lattice,V, animate = False):
     """
+    ===========================================================================
+    Main file for the simple split operator tight binding solver
+    ===========================================================================
+
+    This function collects all modules which define the lattice, the initial
+    wavepacket, the Tight Binding Hamiltonian and the full split operator
+    solver. It applies the same parameters to each module and allows the program
+    to be run from one place.
+
+    The following variables are defined on lines ___ to ___:
+    the size of the system, the correlation length, the
+    width of the Gaussian packet, the momentum eigenvalues in x and y and the
+    time intervals and length to be simulated can be adjusted. These can be
+    changed to simulate different conditions.
+
+    Inputs
+    -----------
+    lattice - string
+            - description of the lattice to be modelled. This can be either
+                'square', 'graphene', or '1D square'.
+
+    V       - string
+            - description of the potential to be used on the system. This can be
+                either 'None', 'one dimensional' or 'two dimensional'.
+                If the lattice is '1D square', the potential MUST be 'None'
+
+    animate - boolean
+            - Determines whether or not a .mp4 file of the propagation will be
+                produced. N.B this takes more time so should only be set if it
+                is necessary to study the propagation.
+
+    Returns
+    ----------
+    N.A
+
+    If 'animate' is set to be true, an .mp4 file will be saved.
+    A plot of the final state of the wavepacket will be displayed as a contour
+        plot where the colour represents the amplitude.
     """
 
-    n = 100
-    m = 100
-    lc = 2
-    s = 5*lc
-    kx = math.pi/(5*lc)
-    ky = math.pi/(5*lc)
-    dt = 0.1e-15
-    T = 4e-15
-    Ns = round(T/dt) + 1
+    # System Parameters
+    m = 100                     # Number of atoms along x
+    n = 100                     # Number of atoms along y
+    lc = 2                      # Correlation length for disorder potential
+    s = 5*lc                    # Width of initial Gaussian wavepacket
+    kx = math.pi/(5*lc)         # Momentum eigenvalue (wavenumber) along x
+    ky = math.pi/(5*lc)         # Momentum eigenvalue (wavenumber) along x
+    dt = 0.1e-15                # Time interval to be sampled
+    T = 4e-15                   # Total time of wavepacket propagation
+    Ns = round(T/dt) + 1        # Integer number of time steps
 
+
+    # Import the simple Hamiltonian solver
     from DD_SS import TB_ss                                 # Solver module
+
 
     if lattice == 'square':
         # Import modules for Square lattice
@@ -131,55 +185,62 @@ def TBS(lattice,V):
         from DD_WP_S import Psi                             # Wavefunction module
         from DD_DP_S import oneDdisorderpotential           # Potential module
         from DD_DP_S2 import twoDdisorderpotential          # Potential module
-        from DD_FH_S import FTBH                             # Hamiltonian module
+        from DD_FH_S import FTBH                            # Hamiltonian module
 
     if lattice == 'graphene':
-        # Import modules for Square lattice
+        # Import modules for Graphene lattice
         from DD_WP_G import Crystal                         # Wavefunction module
         from DD_WP_G import Psi                             # Wavefunction module
         from DD_DP_G import oneDdisorderpotential           # Potential module
         from DD_DP_G2 import twoDdisorderpotential          # Potential module
-        from DD_FH_G import FTBH                             # Hamiltonian module
+        from DD_FH_G import FTBH                            # Hamiltonian module
 
+    if lattice == '1D square':
+        # Import modules for 1D square lattice
+        # Must have no potential for this 1D solver
+        from DD_1D_modified import Crystal                  # Crystal Module
+        from DD_1D_modified import Psi                      # Wavefunction Module
+        from DD_1D_modified import TBH                      # Hamiltonian Module
+        from DD_1D_modified import TB_ss                    # Solver Module
+
+
+    # Run the system modules to descibe the lattice and initial wavepacket
     pos = Crystal(n,m)
     wfc = Psi(s,kx,ky,n,m,pos)
-    DP = oneDdisorderpotential(m,n,lc,pos)
+
+    # Run the potential module for the system to be simulated
+    if V == 'None':
+        # If the system is to be run with no external potential, assign an
+        # arbitrary value of 1 to the potential variable.
+        DP = 1
+
+    if V == 'one dimensional':
+        DP = oneDdisorderpotential(m,n,lc,pos)
 
     if V == 'two dimensional':
         DP = twoDdisorderpotential(m,n,lc,pos)
 
+    # Run the Hamiltonian module for the system
     FH = FTBH(DP,n,m,dt,V)
 
-    #Change this to True to produce an mp4 video
-    animate = True
-
     if animate:
-
+    # Solves the tight binding hamiltonian using a split operator method
+    # described in the file DD_TB_S. Records an image after each time step and
+    # compiles into an mp4 movie file.
         from animate import MakeMovie
 
         pd = TB_ss(n,m,pos,wfc,FH,T,dt,animate)
         MakeMovie('Tight Binding in ' + lattice + ' with ' + V + ' potential')
     else:
-
-
-    #why does the solver take the DP argument?
+    # Solves the tight binding hamiltonian using a split operator method
+    # described in the file DD_TB_S.
+    # why does the solver take the DP argument?
         pd = TB_ss(n,m,pos,wfc,FH,T,dt)
 
     plt.contourf(pos[0].reshape((n,m)),pos[1].reshape((n,m)), pd, 100, cmap = 'gnuplot')
     plt.title('n='+str(n)+' m='+str(m)+' t='+str(Ns*0.1)+'fs')
     plt.show()
 
-    # #Change this to True to produce an mp4 video
-    # animate = False
-    #
-    # if animate:
-    #
-    #     from animate import MakeMovie
-    #
-    #     TB_ss(DP,n,m,pos,wfc,H,T,dt)
-    #     MakeMovie('Tight Binding in ' + lattice + ' with ' + potential + ' potential')
-    # else:
-    #     TB_ss(DP,n,m,pos,wfc,H,T,dt)
 
 if __name__ == '__main__':
-    TBS('graphene', 'one dimensional')
+    TB('graphene', 'one dimensional')
